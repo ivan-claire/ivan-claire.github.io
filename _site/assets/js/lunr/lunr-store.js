cabe80dc5e18d19b6004c32951d71f2312c3a449
ivan-claire <ngongivan@gmail.com>
2020-09-06 22:00:17 +0200

    new changes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "PATE Framework",
-        "excerpt":"Do you want to join the hospital team in implementing the classifier for COVID19 through Differential Privacy?. Then head over to this here to join them. You will find a coding tutorial that shows a step by step hands on implementation of the classifier. The Github repository can be found...","categories": ["comic"],
+        "title": "Core Concepts",
+        "excerpt":"Do you want to join the hospital team in implementing the classifier for COVID19 through Differential Privacy?. Then head over to this here to join them. You will find a coding tutorial that shows a step by step hands on implementation of the classifier. The Github repository can be found...","categories": ["kubernetes"],
         "tags": [],
-        "url": "http://localhost:4000/comic/pate/",
+        "url": "http://localhost:4000/kubernetes/kube-core-concepts/",
         "teaser": "http://localhost:4000/assets/images/pate.png"
       },]
